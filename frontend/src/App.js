94fe3a2f562c3f444720591ea7dae88f8c509c45
raghadm7md <raghad.m7md.1417@gmail.com>
2021-02-17 15:14:13 +0300

    appointment card and register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,49 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Profile from "./Profile";
+import Register from "./Register"
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointment: [
+        {
+          title: "fix computer",
+          date: "15 feb",
+          time: "9:00 am",
+          tech_man: "ali",
+        },
+        {
+          title: "fix computer",
+          date: "15 feb",
+          time: "10:00 am",
+          tech_man: "ali",
+        },
+        {
+          title: "fix computer",
+          date: "15 feb",
+          time: "11:00 am",
+          tech_man: "ali",
+        },
         
-    </div>
-  );
-}
+      ],
+    };
+  }
+  addAppointment = () => {
 
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Profile info={this.state.appointment}></Profile>
+        <button onClick={this.addAppointment}>add appointment</button>
+        <div className="RegisterForm">
+        <Register/>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ frontend/src/Profile.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Card, Button, Col, Row } from "antd";
+
+export default class profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointment: this.props.info,
+    };
+  }
+
+  deleteAppointment = (index) => {
+    // logic error we will delete it from DB 
+    let appointment = this.state.appointment;
+    appointment.splice(index, 1);
+    this.setState({ appointment: appointment });
+  };
+
+  render() {
+    console.log(this.props.info);
+    console.log(this.state.appointment);
+    const appointment = this.state.appointment.map((element, index) => {
+      return (
+        <Card style={{ width: 300 }}>
+          <p>{element.title}</p>
+          <p>{element.date}</p>
+          <p>{element.time}</p>
+          <p>{element.tech_man}</p>
+          <Button>Track</Button>
+          <Button>Edit</Button>
+          <Button
+            variant="outline-primary"
+            onClick={(event) => {
+              this.deleteAppointment(event, index);
+            }}
+          >
+            Delet
+          </Button>
+        </Card>
+      );
+    });
+    return (
+      <div>
+        <h1>hi from profile</h1>
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+              {appointment}
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
