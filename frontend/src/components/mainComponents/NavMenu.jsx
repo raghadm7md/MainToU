1f89c888aa608f9c20eb5ebb893285d27b9117a0
05_Abeer Win_SEI-14 <albawardi.as@gmail.com>
2021-02-19 23:22:37 +0300

    about route > fixed (#18)

--- frontend/src/components/mainComponents/NavMenu.jsx
+++ frontend/src/components/mainComponents/NavMenu.jsx
@@ -5,21 +5,18 @@ import "../../App.css";
 
 function NavMenu() {
   return (
-    <Menu
-      mode="horizontal"
-      className="navMenu"
-    >
+    <Menu mode="horizontal" className="navMenu">
       <Menu.Item key="home">
         <Link to="/">Home</Link>
       </Menu.Item>
       <Menu.Item key="claendar">
         <Link to="/calendar">Calendar</Link>
       </Menu.Item>
-      <Menu.Item key="profile">
-        <Link to="/profile">Profile</Link>
-      </Menu.Item>
       <Menu.Item key="about">
         <Link to="/about">About</Link>
+      </Menu.Item>
+      <Menu.Item key="profile">
+        <Link to="/profile">Profile</Link>
       </Menu.Item>
     </Menu>
   );
